21079d256aec92a463b247bf9436ed839dc3ac60
over300laughs <over300laughs@gmail.com>
2015-09-04 23:07:49 -0600

    cleaned house a bit

--- Excuse.php
+++ Excuse.php
@@ -10,86 +10,30 @@ class Excuse_Generator {
 		// if($rainbowResults) {
 		// 	return 
 		// }
+
 		$scenario = strtolower($scenario);
-		$person = self::get_person($scenario);
+		$person = self::find_person($scenario);
+		$subject = self::find_subject($scenario);
+		echo $subject;
+		exit;
+
 	}
 
-	//Just starting simple
-	function rainbow($scenario){
-		//check context using rainbow array first
-		$rainbow = array(
-			"a" => "i'm late for work",		
-			"d" => "won't leave me alone",		
-			"e" => "don't want to go",
-			"f" => "date",
-			"g" => "didn't finish",
-			"h" => "forgot about",
-			"i" => "forgot to pick up",
-			"j" => "avoid a",
-			"l" => "leave early",
-			"m" => "showed up late",
-			"n" => "never showed",
-			"o" => "stop going",
-			"p" => "stopped",
-			"q" => "leave a conversation",
-		);
-
-		$exact_match = array();
-		foreach($rainbow as $key => $snippet) {
-			if($scenario == $snippet) {
-				$rainbow_answer = get_rainbow_answer($snippet);
-				return $rainbow_answer;
-			}
-		}
-		return false;
-	}
-
-	function get_person($scenario) {
-		$persons = array(
-			"wife",
-			"husband",
-			"friend",
-			"friends",
-			"mom",
-			"dad",
-			"sister",
-			"brother",
-			"aunt",
-			"uncle",
-			"cousin",
-			"grandpa",
-			"grandma",
-			"g-pa",
-			"g-ma",
-			"great gran",
-			"girlfriend",
-			"boyfriend",
-			"lover",
-			"lover",
-			"family",
-			"families",
-			"partner",
-			"boss",
-			"co-worker",
-			"coworker",
-			"employee",
-			"manager",
-			"customer",
-			"client",
-			"consultant",
-			"therapist",
-			"chef",
-		);
-
-		$tell = array(
+	function find_person($scenario) {
+		
+		//TODO: take into account present tense such as whispers as well as the such as officer
+		//adjectives still not accounted for :(
+		$tell = array (
 			"said to ",
-			"tell to ",
+			"spoke to ",
+			"whispered to ",
+			"say to ",
+			//"tell to ", shouldn't need this
 			"told ",
-			"told my ",
+			//"told my ", shouldn't need this
 			"tell ",
 			"explain to ",
 			"mentioned to ",
-			"say to ",
 		);
 
 		//confirm recipient
@@ -108,24 +52,14 @@ class Excuse_Generator {
 				$directed_person = $scen_ex[0];
 
 				//likely only going to be one we hope. 
-				if($scen_ex[0] == "my" || $scen_ex[0] == "to");
+				if($scen_ex[0] == "my" || $scen_ex[0] == "to")
 					$directed_person = $scen_ex[1];
 
 				return $directed_person;
  			}
 		}
 
-
-		/*** the world is not ready for this yet**/
-		//try again for person if we somehow missed them
-		// $persons_found = array();
-		// foreach($persons as $snippet) {
-		// 	if(strpos($scenario, $snippet) !== false)
-		// 		$persons_found = $snippet;
-		// }
-		//$person = $snippets_found[0];
-
-		//someone was likely not mentioned check for pronouns
+		//someone was likely not mentioned so check for pronouns
 		$pronouns = array(
 			"he",
 			"she",
@@ -143,102 +77,55 @@ class Excuse_Generator {
 			}
  		}
 
+ 		// use levenshtein
+ 		// double check with metaphone
+
  		//let's assume they didn't refer to anyone
  		return false;
 	}
 
-	function analyze($scenario, $rainbowResults){
-		$subject_words = array(
-			"work",
-			"school",
-			"house",
-			"party",
-			"funeral",
-			"a date",
-			"breakfast",
-			"lunch",
-			"dinner",
-			"ticket",
-			"hospital",
-			"church",
-			"game",
-			"concert",
-			"class",
-			"meeting",
-			"movies",
+	function find_subject($scenario) {
+		$english_constructs = array(
+			"go to ",
+			"going to ",
+			"late for ",
+			"asking me on ",
+			"asking me for ",
+			"asking me to attend ",
+			"get out of ",
+			"avoid ",
 		);
+		$construct_found = array();
+		foreach($english_constructs as $construct) {
+			if(strpos($scenario, $construct) !== false){
+				$construct_found[] = $construct;
+				break 2; //break to improve performance
+				//TODO bring in second foreach for performance
+			}
+		}
+		if(!empty($construct_found)) {
+			foreach($construct_found as $constrct) {
+				$position = strpos($scenario, $constrct);
+				$position += strlen($constrct);
+				$clip = substr($scenario, $position);
+				$scen_ex = explode(" ", $clip);
 
-		$action_words = array(
-			"late",
-			"in time",
-			"missed",
-			"don't want to",
-			"can't go",
-			"can't make it",
-			"forgot to",
-			"forgot about",
-			"pay",
-			"make it",
-			"can't",
-			"never showed up",
-			"get out",
-			"injured",
-			"said I would",
-			"not",
-			"never",
-			"made it",
-			"couldn't",
-			"on time",
-		);
-
-
-		// $persons = array(
-		// 	"wife",
-		// 	"husband",
-		// 	"friend",
-		// 	"friends",
-		// 	"mom",
-		// 	"dad",
-		// 	"sister",
-		// 	"brother",
-		// 	"aunt",
-		// 	"uncle",
-		// 	"cousin",
-		// 	"grandpa",
-		// 	"grandma",
-		// 	"g-pa",
-		// 	"g-ma",
-		// 	"great gran",
-		// 	"girlfriend",
-		// 	"boyfriend",
-		// 	"lover",
-		// 	"lover",
-		// 	"family",
-		// 	"families",
-		// 	"partner",
-		// 	"boss",
-		// 	"co-worker",
-		// 	"coworker",
-		// 	"employee",
-		// 	"manager",
-		// 	"customer",
-		// 	"client",
-		// 	"consultant",
-		// 	"therapist",
-		// 	"chef",
-		// );
-
-		$segways = array(
-			"Just tell ",
-			"Just tell them ",
-			"I would just say ",
-			"Just explain that ",
-			"It would probably go over best to say ",
-			"I would just mention that you ",
-		);
-
-
+				//likely at the end of the sentence
+				//has room for one adjective
+				$prep_words = array(
+					"a",
+					"an",
+					"the",
+				); 
+				if(in_array($prep_words, $scen_ex) && count($scen_ex) <= 1) {
+					$subject = $scen_ex[1];
+					return $subject;
+				}
+ 			}
+ 		}
+ 		return false;
 	}
+
 
 	public $exc1 = "Just say traffic was terrible.";
 	public $exc2 = "Just say your arm cramped up.";
@@ -247,35 +134,6 @@ class Excuse_Generator {
 	public $exc5 = "I never do that. Its way too time consuming and besides its so passe. I would just say I took my dog to the vet and found out she's having puppies. Its boys and girls! Tell them you like their name and say you'll be naming one after them.";
 	public $exc6 = "You're one of those! Speeding is never a good thing. If you're not packing donuts then tell the officer you always drive that fast and nobody has said anything to you before.";
 }
-
-		// $exact_match = array();
-		// foreach($rainbow as $key => $snippet) {
-		// 	if(strpos($snippet, $scenario) !== false) {
-		// 		$snippets_found[$key] = $snippet;
-		// 	}
-		
-
-
-		// $rainbow = array(
-		// 	"a" => "late for work",		
-		// 	"b" => "dropped a knife",		
-		// 	"c" => "have to go to a",		
-		// 	"d" => "won't leave me alone",		
-		// 	"e" => "don't want to go",
-		// 	"f" => "date",
-		// 	"g" => "didn't finish",
-		// 	"h" => "forgot about",
-		// 	"i" => "forgot to",
-		// 	"j" => "avoid a",
-		// 	"k" => "how can I say",
-		// 	"l" => "leave early",
-		// 	"m" => "showed up late",
-		// 	"n" => "never showed",
-		// 	"o" => "stop going",
-		// 	"p" => "stopped",
-		// 	"q" => "leave a conversation",
-		// );
-
 
 ?>
 
